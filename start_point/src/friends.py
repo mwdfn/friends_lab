02ed0f258850aab81255834b1145a18fa243208f
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-03-31 16:12:15 +0100

    first commit

---
+++ start_point/src/friends.py
@@ -0,0 +1,44 @@
+
+# Question 1
+from pickle import FALSE
+
+
+def get_name(person):
+    return person["name"]
+
+# Quesiton 2
+def get_favourite_tv_show(person):
+    return person["favourites"]["tv_show"]
+
+# Question 3
+# def likes_food(person2):
+#     if ["favourite"] ["snacks"] == "bread":
+#         return person2 = True 
+
+def likes_to_eat(person, food):
+
+    foundFood = False
+    snacks = person["favourites"]["snacks"]
+
+    for snack in snacks:
+        if snack == food:
+         foundFood = True
+    return foundFood
+
+def add_friend(person, new_friend):
+    person ["name"].append(new_friend)
+len(2(person["friends"]))
+
+
+
+# def likes_to_eat(person, food):
+#     snacks = person["favourites"]["snacks"]
+
+#     for snack in snacks:
+#         if snack != food:
+#             return False
+      
+
+
+
+
